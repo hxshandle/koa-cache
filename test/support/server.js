0a0358d3402b41b06c54a05789ae7af5f719ce8f
Handle Huang <handle.huang0409@gmail.com>
2015-08-17 16:39:52 +0800

    add memory cache

--- test/support/server.js
+++ test/support/server.js
@@ -1,13 +1,30 @@
 'use strict';
 
 var koa = require('koa');
+var debug = require('debug')('koa-cache:test-server');
 var http = require('http');
+var json = require('koa-json');
+var cache = require('../../');
+var bodyParser = require('koa-bodyparser');
+
 var app = koa();
+
+app.use(cache());
+app.use(bodyParser());
+app.use(json());
 
 app.use(function *controllers(){
   switch(this.request.path){
     case '/favicon.ico':
       this.status = 404;
+      break;
+    case '/getCache':
+      debug('body is ',this.request.body);
+      var t = this;
+      var data = yield this.cache.get("getCache",function(){
+        return t.request.body;
+      });
+      this.body = data;
       break;
     default:
       this.body = "do nothing...";
--- test/test.cache.js
+++ test/test.cache.js
@@ -1,16 +1,23 @@
 "use strict";
 
-var request = require('supertest');
-var app = require('./support/server');
-
+var request = require('supertest'),
+    app = require('./support/server')
+    should = require('should');
+var date = new Date().getTime();
+var FIELD_NAME = "dateTime";
 describe('koa-cache-test', function () {
-  it('should do what...', function (done) {
+
+  it('getDate from cache', function (done) {
     request(app)
-    .get('/favicon.ico')
-    .expect(404)
-    .end(function(err, res){
-      if(err) return done(err);
-      done();
-    });
+    .post('/getCache')
+    .send({ dateTime: date})
+    .expect({dateTime:date},done);
+  });
+
+  it('getDate should return cached date time', function (done) {
+    request(app)
+    .post('/getCache')
+    .send({ dateTime: "1234"})
+    .expect({dateTime:date},done);
   });
 });
